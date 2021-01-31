d629641d3f8193a1f53a59d64f3e385ca63726d6
brunu97 <brunuo@live.com.pt>
2021-01-31 01:17:24 +0000

    NotasApp

---
+++ public/js/index.js
@@ -0,0 +1,108 @@
+
+$(document).ready(async function() {
+    fetch("/versessao").then(async (resposta) => {  
+        dados = await resposta.json()
+        if (dados.valido == 1) { // Existe uma sessão
+            document.getElementById("sec_login").style.display = "none";
+            document.getElementById("sec_notas").style.display = "block";
+            obtemNotas();
+        } else { // User não tem sessão aberta
+            document.getElementById("sec_login").style.display = "block";
+            document.getElementById("sec_notas").style.display = "none";
+        }
+    });
+});
+
+async function obtemNotas(){
+    fetch("/notas").then(async (resposta) => {
+        dados = await resposta.json()
+        for (k in dados){
+            div = document.getElementById("listasNotas");
+            nota = document.createElement("p");
+            nota.innerHTML = dados[k]["Texto"];
+            nota.id = dados[k]["idNota"];
+            nota.classList.add("apagaNota");
+            div.appendChild(nota)
+        }
+    });
+}
+
+$(document).on("click","#adicionaNota", function() {
+    let textoNota = document.getElementById("input_nota").value;
+    document.getElementById("input_nota").value = ""
+    fetch("/novanota", {
+        headers: { "Content-Type": "application/json", },
+        method: "POST",
+        body: JSON.stringify({
+          texto: textoNota,
+        }),
+    }).then(async (resposta) => {
+        document.getElementById("listasNotas").innerHTML = "";
+        dados = await resposta.json()
+        
+        for (k in dados){
+            div = document.getElementById("listasNotas");
+            nota = document.createElement("p");
+            nota.innerHTML = dados[k]["Texto"];
+            nota.id = dados[k]["idNota"];
+            nota.classList.add("apagaNota");
+            div.appendChild(nota)
+        }
+    });
+});
+
+
+$(document).on("click","#loginbnt", function() {
+    let nome = document.getElementById("input_usernome").value;
+    let senha = document.getElementById("input_usersenha").value;
+
+    fetch("/login", {
+        headers: { "Content-Type": "application/json", },
+        method: "POST",
+        body: JSON.stringify({
+          u_nome: nome,
+          u_senha: senha,
+        }),
+    }).then(async (resposta) => {
+        dados = await resposta.json()
+
+        if (dados.valido == 1){
+            $("#sec_login").fadeOut(100, function() { 
+                $("#sec_notas").fadeIn(350)
+                document.getElementById("input_usernome").value = ""
+                document.getElementById("input_usersenha").value = ""
+                document.getElementById("sec_login").style.backgroundColor = "#7497d8ad"
+
+                obtemNotas();
+           });
+        } else {
+            document.getElementById("sec_login").style.backgroundColor = "red"
+        }
+    });
+});
+
+
+$(document).on("click",".apagaNota", function() { // Apaga notas ao clicar no texto
+    let id = $(this).attr('id')
+    document.getElementById(id).remove();
+
+    fetch("/removenota", {
+        headers: { "Content-Type": "application/json", },
+        method: "POST",
+        body: JSON.stringify({
+          notaId: id,
+        }),
+    }).then(async (resposta) => {
+        document.getElementById("listasNotas").innerHTML = "";
+        dados = await resposta.json()
+        
+        for (k in dados){
+            div = document.getElementById("listasNotas");
+            nota = document.createElement("p");
+            nota.innerHTML = dados[k]["Texto"];
+            nota.id = dados[k]["idNota"];
+            nota.classList.add("apagaNota");
+            div.appendChild(nota)
+        }
+    });
+});
---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require("express");
+var bodyParser = require('body-parser')
+const expresssSessao = require("express-session")
+const app = express();
+
+const PORTA = 5000;
+var jsonParser = bodyParser.json()
+
+app.use(expresssSessao({
+    resave: false,
+    saveUninitialized: false,
+    name: "sessaoID", // Nome do Utilizador
+    secret: "7986292486187982",
+    cookie: {
+      maxAge: 60000, // 1 Minuto
+      sameSite: true,
+      httpOnly: true
+    }
+  }))
+
+var Utilizadores = []
+function utilizador(n, s) {
+     this.nome = n;
+     this.senha = s;
+     this.Notas = [];
+}
+
+var user = new utilizador("1", "1"); // cria um Utilizador de testes
+Utilizadores.push(user);
+
+function obtemNotasUser(nomeUser) {
+    for (k in Utilizadores){
+        if (Utilizadores[k].nome == nomeUser) {
+            return Utilizadores[k].Notas;
+        }
+    }
+}
+
+app.use(express.static("public"));
+
+app.get("/notas", (req, res) => {
+    if (req.session.sessaoID) {
+        res.send(obtemNotasUser(req.session.sessaoID))
+    }
+});
+
+app.get("/versessao", jsonParser, (req, res) => {
+    if (req.session.sessaoID) {
+        res.json({valido: 1})
+    } else {
+        res.json({valido: 0})
+    }
+});
+
+app.post("/login", jsonParser, (req, res) => {
+    
+    for (k in Utilizadores){
+        if (Utilizadores[k].nome == req.body.u_nome && Utilizadores[k].senha == req.body.u_senha) {
+            req.session.sessaoID = req.body.u_nome;
+            res.json({valido: 1})
+            return;
+        }
+    }
+    res.json({valido: 0})
+});
+
+app.post("/removenota", jsonParser, (req, res) => { // REMOVE NOTAS
+    if (req.session.sessaoID) {
+       
+        obtemNotasUser(req.session.sessaoID).splice(req.body.notaId, 1)
+
+        // Volta a organizar as notas para ajustar o ID
+        for (i = 0; i < obtemNotasUser(req.session.sessaoID).length; i++) {
+            obtemNotasUser(req.session.sessaoID)[i].idNota = i.toString();
+        }
+        res.send(obtemNotasUser(req.session.sessaoID))
+    }
+});
+
+app.post("/novanota", jsonParser, (req, res) => {
+    if (req.session.sessaoID) {
+        obtemNotasUser(req.session.sessaoID).push({Texto: req.body.texto, idNota: "0"}) // Insere nota
+ 
+        // adiciona Ids ás notas
+        for (i = 0; i < obtemNotasUser(req.session.sessaoID).length; i++) {
+            obtemNotasUser(req.session.sessaoID)[i].idNota = i.toString();
+        }
+        res.send(obtemNotasUser(req.session.sessaoID))
+       
+    }
+});
+
+
+app.listen(PORTA);
+console.log("Server Ligado");
